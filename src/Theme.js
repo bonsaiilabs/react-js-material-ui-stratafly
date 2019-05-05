d44a7e55d416fcb2ed67dffb36ea9c007210a7d5
deekshasharma <deeksha.sharma25@gmail.com>
2019-05-05 18:32:51 -0400

    Created Strata Theme

---
+++ src/Theme.js
@@ -0,0 +1,28 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const Theme = createMuiTheme({
+  palette: {
+    primary: {
+      700: '#119620',
+      500: '#2FB735',
+      100: '#C5E9C4',
+      200: '#A0DB9E',
+      main: '#008512',
+      light: '#E7F6E7',
+      contrastText: '#FFFFFF'
+    },
+    secondary: {
+      main: '#000000'
+    },
+    text: {
+      primary: 'rgba(255, 255, 255, 1)',
+      primaryMediumEmphasis: 'rgba(255, 255, 255, 0.6)',
+      secondary: 'rgba(0, 0, 0, 1)',
+      secondaryMediumEmphasis: 'rgba(0, 0, 0, 0.6)'
+    }
+  },
+  typography: {
+    useNextVariants: true,
+    subtitle1: { fontWeight: 500 }
+  }
+});
