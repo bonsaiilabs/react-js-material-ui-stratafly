ca5544ce5259efb76b47d66d4eb8caa2a2a13cf5
deekshasharma <deeksha.sharma25@gmail.com>
2019-05-05 18:49:51 -0400

    Added MuiThemeProvider to wrap all the App components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import AppHeader from './components/AppHeader';
+import {Theme} from "./Theme";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 
 class App extends Component {
   render() {
-    return <AppHeader/>;
+    return (
+      <MuiThemeProvider theme={Theme}>
+        <AppHeader />
+      </MuiThemeProvider>
+    );
   }
 }
 export default App;
