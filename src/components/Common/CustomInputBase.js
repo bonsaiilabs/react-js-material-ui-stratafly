0e26d4471d85cdbb4ff36121cd800921b6598ba5
deekshasharma <deeksha.sharma25@gmail.com>
2019-05-06 16:05:50 -0400

    Added a CustomInputBase component

---
+++ src/components/Common/CustomInputBase.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import InputBase from '@material-ui/core/InputBase';
+
+function CustomInputBase({ classes, onChange, value, placeholder, icon, type }) {
+  let placeholderField = type === 'date' ? 'Date' : placeholder;
+  return (
+    <InputBase
+      startAdornment={
+        <InputAdornment position="start" className={classes.icon}>
+          {icon}
+        </InputAdornment>
+      }
+      fullWidth={true}
+      placeholder={placeholderField}
+      value={value}
+      type={type}
+      className={classes.input}
+      onChange={event => onChange(event.target.value)}
+    />
+  );
+}
+
+const styles = theme => ({
+    input: {
+        backgroundColor: theme.palette.primary.main,
+        borderRadius: theme.shape.borderRadius * 2,
+        color: theme.palette.common.white,
+        fontSize: theme.typography.subtitle1.fontSize,
+        fontWeight: theme.typography.subtitle1.fontWeight
+    },
+    icon: {
+        paddingLeft: theme.spacing.unit
+    }
+});
+
+export default withStyles(styles)(CustomInputBase);
