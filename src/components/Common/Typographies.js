7e7ae383f62a1dd389ea0c98914c731c394c2be4
deekshasharma <deeksha.sharma25@gmail.com>
2019-05-06 15:45:24 -0400

    Code reformat

--- src/components/Common/Typographies.js
+++ src/components/Common/Typographies.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
-
 export const Subtitle1White = ({ children }) => {
-    return (
-        <Typography variant={'subtitle1'} color='textPrimary'>
-            {children}
-        </Typography>
-    );
+  return (
+    <Typography variant={'subtitle1'} color="textPrimary">
+      {children}
+    </Typography>
+  );
 };
--- src/components/SearchForm/Traveller.js
+++ src/components/SearchForm/Traveller.js
@@ -3,7 +3,7 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import IconButton from '@material-ui/core/IconButton';
 import TravellerIcon from '@material-ui/icons/PersonOutlined';
 import Grid from '@material-ui/core/Grid';
-import {Subtitle1White} from "../Common/Typographies";
+import { Subtitle1White } from '../Common/Typographies';
 
 const Traveller = props => {
   const { classes, onClickTraveller, totalTravellers } = props;
@@ -11,17 +11,17 @@ const Traveller = props => {
     <Grid container alignItems="center" className={classes.root} onClick={onClickTraveller}>
       <Grid item>
         <IconButton color="inherit" aria-label="Traveller">
-          <TravellerIcon className={classes.iconLeft}/>
+          <TravellerIcon className={classes.iconLeft} />
           <Subtitle1White>{totalTravellers}</Subtitle1White>
         </IconButton>
       </Grid>
     </Grid>
   );
-}
+};
 
 const styles = theme => ({
   root: { flexGrow: 1 },
-  iconLeft: { color: theme.palette.common.white },
+  iconLeft: { color: theme.palette.common.white }
 });
 
 export default withStyles(styles)(Traveller);
