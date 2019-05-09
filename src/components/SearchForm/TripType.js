addec97e8099c752a8cf684f75896bab08fa8304
deekshasharma <deeksha.sharma25@gmail.com>
2019-05-09 06:42:08 -0400

    Removed FormControl component

--- src/components/SearchForm/TripType.js
+++ src/components/SearchForm/TripType.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Radio from '@material-ui/core/Radio';
-import { FormControl, RadioGroup } from '@material-ui/core';
+import { RadioGroup } from '@material-ui/core';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { trip } from '../../shared/app-constants';
 import { Subtitle1White } from '../Common/Typographies';
@@ -10,20 +10,18 @@ const TripType = props => {
   const { classes, selectedRadio, onChangeRadio } = props;
   let isOneWay = selectedRadio === 'one-way';
   return (
-    <FormControl component="fieldset">
-      <RadioGroup value={selectedRadio} onChange={onChangeRadio} row>
-        <FormControlLabel
-          value="round-trip"
-          label={<Subtitle1White>{trip.roundTrip}</Subtitle1White>}
-          control={<Radio checked={!isOneWay} classes={{ root: classes.root, checked: classes.checked }} />}
-        />
-        <FormControlLabel
-          value="one-way"
-          label={<Subtitle1White>{trip.oneWay}</Subtitle1White>}
-          control={<Radio checked={isOneWay} classes={{ root: classes.root, checked: classes.checked }} />}
-        />
-      </RadioGroup>
-    </FormControl>
+    <RadioGroup value={selectedRadio} onChange={onChangeRadio} row>
+      <FormControlLabel
+        value="round-trip"
+        label={<Subtitle1White>{trip.roundTrip}</Subtitle1White>}
+        control={<Radio checked={!isOneWay} classes={{ root: classes.root, checked: classes.checked }} />}
+      />
+      <FormControlLabel
+        value="one-way"
+        label={<Subtitle1White>{trip.oneWay}</Subtitle1White>}
+        control={<Radio checked={isOneWay} classes={{ root: classes.root, checked: classes.checked }} />}
+      />
+    </RadioGroup>
   );
 };
 
