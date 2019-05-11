00902494fb0831f232896d0b0d16d49a7718d0bb
deekshasharma <deeksha.sharma25@gmail.com>
2019-05-11 06:33:20 -0400

    Added age in PrimaryMediumEmphasis

--- src/components/SearchForm/TravellerDialog/TravellerRows/TravellerWithAge.js
+++ src/components/SearchForm/TravellerDialog/TravellerRows/TravellerWithAge.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
 
-//TODO //Age is in blackMediumEmphasis body2
 export const TravellerWithAge = props => {
-  const { type, age } = props;
+  const { type, age, classes } = props;
   return (
     <Grid container alignItems="center">
       <Grid item xs={12}>
@@ -13,10 +13,14 @@ export const TravellerWithAge = props => {
         </Typography>
       </Grid>
       <Grid item xs={12}>
-          <Typography variant="body2" color="textPrimary">
-              {age}
-          </Typography>
+        <Typography variant="body2" className={classes.age}>
+          {age}
+        </Typography>
       </Grid>
     </Grid>
   );
 };
+
+const styles = theme => ({ age: { color: theme.palette.text.primaryMediumEmphasis } });
+
+export default withStyles(styles)(TravellerWithAge);
