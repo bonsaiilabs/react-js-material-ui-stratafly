380b3dd966a2566fd2d8da12ed1b88d79a1101f7
deekshasharma <deeksha.sharma25@gmail.com>
2019-05-15 11:01:05 -0400

    Transition added to StrataFullScreenDialog

--- src/components/Common/StrataFullScreenDialog.js
+++ src/components/Common/StrataFullScreenDialog.js
@@ -1,36 +1,32 @@
 import React from 'react';
-import withStyles from '@material-ui/core/styles/withStyles';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import Toolbar from '@material-ui/core/Toolbar';
 import Dialog from '@material-ui/core/Dialog';
 import ArrowBack from '@material-ui/icons/ArrowBack';
 import { AppBar, Collapse } from '@material-ui/core';
+import Slide from '@material-ui/core/Slide';
 
-function FullScreen({ classes, label, open, onBack, children }) {
-    return (
-        <Collapse in={true}>
-            <Dialog fullScreen open={open} onClose={onBack} classes={{ paper: classes.root }}>
-                <AppBar position="fixed">
-                    <Toolbar>
-                        <IconButton color="inherit" onClick={onBack} aria-label="Close">
-                            <ArrowBack />
-                        </IconButton>
-                        <Typography variant="h6" color="inherit">
-                            {label}
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-                {children}
-            </Dialog>
-        </Collapse>
-    );
+export const StrataFullScreenDialog = ({ classes, label, open, onBack, children }) => {
+  return (
+    <Collapse in={true}>
+      <Dialog fullScreen open={open} onClose={onBack} TransitionComponent={Transition}>
+        <AppBar position="fixed">
+          <Toolbar>
+            <IconButton color="inherit" onClick={onBack} aria-label="Close">
+              <ArrowBack />
+            </IconButton>
+            <Typography variant="h6" color="inherit">
+              {label}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        {children}
+      </Dialog>
+    </Collapse>
+  );
+};
+
+function Transition(props) {
+  return <Slide direction="left" {...props}/>;
 }
-
-const styles = theme => ({
-    root: {
-        backgroundColor: theme.palette.primary.light
-    }
-});
-
-export const StrataFullScreenDialog = withStyles(styles)(FullScreen);
