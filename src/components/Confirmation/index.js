55da57f31067c721e43cb237369425a5f61628e6
deekshasharma <deeksha.sharma25@gmail.com>
2019-05-16 11:22:39 -0400

    Added bookingConfirmation image and code for Confirmation component

---
+++ src/components/Confirmation/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import withStyles from '@material-ui/core/styles/withStyles';
+import Button from '@material-ui/core/Button';
+import {Typography} from "@material-ui/core";
+import BookingConfirmationIcon from '../../images/bookingcomplete.svg';
+
+
+const Confirmation = ({ classes, onClick }) => {
+  return (
+    <Grid container direction="column" alignItems="center" justify="center" spacing={24} className={classes.root}>
+      <Grid item className={classes.thanks}>
+          <Typography variant="h5">{'Your flight is booked!'}</Typography>
+        {/*<BlackH5 text="Your flight is booked!" />*/}
+      </Grid>
+
+      <Grid item className={classes.thanks}>
+        <Grid container>
+          <Grid item xs={12}>
+            <img src={BookingConfirmationIcon} alt={'booking-confirmation'} height={'100%'} width={'100%'} />
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid item className={classes.thanks}>
+          <Typography variant="subtitle2">{'Check your email for itinerary details'}</Typography>
+        {/*<BlackSubtitle2MediumEmphasis text={'Check your email for itinerary details'} />*/}
+      </Grid>
+
+      <Grid item>
+        <Button onClick={onClick} variant="outlined" color="primary" className={classes.button}>
+          + BOOK FLIGHT
+        </Button>
+      </Grid>
+    </Grid>
+  );
+}
+
+const styles = theme => ({
+  root: { flexGrow: 1, padding: theme.spacing.unit * 3 },
+  thanks: { marginTop: theme.spacing.unit * 8 },
+  button: { marginTop: theme.spacing.unit * 3 }
+});
+export default withStyles(styles)(Confirmation);
