bc838e48c4de175ab388c3c1804a546cf913e0d3
deekshasharma <deeksha.sharma25@gmail.com>
2019-05-17 06:18:44 -0400

    Added new logo and favicon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
@@ -8,13 +8,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Welcome to Stratafly team</p>
+          <h5>Learn Material Design with React</h5>
           <a
             className="App-link"
             href="https://bonsaiilabs.thinkific.com"
             target="_blank"
             rel="noopener noreferrer"
-            style={{ color: '#F8E013' }}
+            style={{ color: '#FFFFFF' }}
           >
             Learn With BonsaiiLabs
           </a>
