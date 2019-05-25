8cb6c7ea046a8fe182c4b17c3c517485ed7795ae
deekshasharma <deeksha.sharma25@gmail.com>
2019-05-25 07:12:36 -0400

    Added HomePage for big screen

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import AppHeader from './components/AppHeader';
 import { Theme } from './Theme';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import SearchForm from './components/SearchForm';
+import './App.css';
+import { HomePage } from './HomePage';
 import Empty from './components/Empty';
 import { formatDate } from './shared/util';
 
 class App extends Component {
   render() {
+    if (window.screen.width >= 1024 && window.screen.height >= 768) return <HomePage />;
     return (
       <MuiThemeProvider theme={Theme}>
         <AppHeader />
