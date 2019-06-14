2e80b6869286aab561c87952fc67df6ff40ad5ef
deekshasharma <deeksha.sharma25@gmail.com>
2019-06-14 06:50:31 -0400

    Search Results component code ready

--- src/shared/app-constants.js
+++ src/shared/app-constants.js
@@ -10,12 +10,6 @@ export const trip = {
   oneWay: 'One way'
 };
 
-export const maxTravellersWarning = 'More than 9 passengers not allowed';
-
-export const infantAloneWarning = 'One adult per infant is required';
-
-export const maxTravellersAllowed = 9;
-
 export const stops = {
   nonStop: 'Nonstop',
   oneStop: '1 stop'
@@ -30,13 +24,31 @@ export const AirlineToIcon = {
   'Southwest Airlines': SouthWest
 };
 
-export const airlineClassToLabel = { economy: 'Economy' };
-
-export const codeToCityName = { 'YVR': 'Vancouver', 'SFO': 'San Francisco'};
-
-export const flightInfo = {
-  legRoom: 'Above average legroom',
-  wifi: 'Wifi onBoard',
-  usb: 'In-seat power and USB outlets',
- delayMessage:  'This flight is often delayed by 30+ minutes'
+export const testFlights = {
+  from: 'YVR',
+  to: 'SFO',
+  date: new Date(),
+  flightsWithFares: [
+    {
+      airline: 'Air Canada',
+      airlineCode: 'AC8840',
+      arrive: '11:13',
+      class: 'economy',
+      depart: '08:40',
+      price: 300,
+      stops: 'Nonstop',
+      totalTime: '2h 33m'
+    },
+    {
+      airline: 'American Airlines',
+      airlineCode: 'AA6072',
+      arrive: '13:15',
+      class: 'economy',
+      depart: '06:44',
+      price: 400,
+      stops: '1 stop',
+      totalTime: '6h',
+      viaAirlineCode: 'LAX'
+    }
+  ]
 };
