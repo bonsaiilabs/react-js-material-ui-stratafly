e81ba2273f2ccb122f578aa2f16d469a1e6281cf
deekshasharma <deeksha.sharma25@gmail.com>
2019-06-20 14:04:16 -0400

    removed tripType

--- src/components/SearchResults/Flights/Flight/index.js
+++ src/components/SearchResults/Flights/Flight/index.js
@@ -5,14 +5,14 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 
 export const Flight = props => {
-  const { flight, tripType, from, to, onSelect, classes } = props;
+  const { flight, from, to, onSelect, classes } = props;
   let nonStop = stops.nonStop;
   let oneStop = stops.oneStop + ' at ' + flight.viaAirlineCode;
   let stop = flight.stops === stops.nonStop ? nonStop : oneStop;
   return (
     <Grid container onClick={() => onSelect(flight)}>
       <Grid item xs={12}>
-        <IconTimePrice fromCode={from} toCode={to} tripType={tripType} flight={flight} classes={classes} />
+        <IconTimePrice fromCode={from} toCode={to} flight={flight} classes={classes} />
       </Grid>
 
       <Grid item xs={12}>
--- src/components/SearchResults/Flights/index.js
+++ src/components/SearchResults/Flights/index.js
@@ -3,12 +3,12 @@ import { Card, Divider } from '@material-ui/core';
 import { Flight } from './Flight';
 
 export const Flights = props => {
-  const { flightsWithFares, tripType, from, to, onSelect, classes } = props;
+  const { flightsWithFares, from, to, onSelect, classes } = props;
   return (
     <Card raised={true} className={classes.card}>
       {flightsWithFares.map((flight, index) => (
         <div key={index}>
-          <Flight flight={flight} tripType={tripType} from={from} to={to} onSelect={onSelect} classes={classes} />
+          <Flight flight={flight} from={from} to={to} onSelect={onSelect} classes={classes} />
           <Divider />
         </div>
       ))}
