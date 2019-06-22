486344d2bfd8346a37b70725e3b4189af034f551
deekshasharma <deeksha.sharma25@gmail.com>
2019-06-22 11:51:25 -0400

    Changes pulled from the previous branch

--- src/components/SearchResults/Flights/index.js
+++ src/components/SearchResults/Flights/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Card, Divider } from '@material-ui/core';
-import { FlightCard } from './FlightCard';
+import { Flight } from './Flight';
 
 export const Flights = props => {
-  const { flightsWithFares, tripType, from, to, onSelect, classes } = props;
+  const { flightsWithFares, from, to, onSelect, classes } = props;
   return (
     <Card raised={true} className={classes.card}>
       {flightsWithFares.map((flight, index) => (
         <div key={index}>
-          <FlightCard flight={flight} tripType={tripType} from={from} to={to} onSelect={onSelect} classes={classes} />
+          <Flight flight={flight} from={from} to={to} onSelect={onSelect} classes={classes} />
           <Divider />
         </div>
       ))}
