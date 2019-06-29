833130991e89e12b31e80fc08d587e024049cb2f
deekshasharma <deeksha.sharma25@gmail.com>
2019-06-29 12:49:22 +0530

    Added util functions

--- src/shared/util.js
+++ src/shared/util.js
@@ -9,3 +9,7 @@ export const getDateToString = date => {
 };
 
 export const isObjectEmpty = obj => Object.entries(obj).length === 0 || typeof obj === 'undefined';
+
+export const isArrayEmpty = arr => arr.length === 0 || typeof arr === 'undefined';
+
+export const isUndefined = any => typeof any === 'undefined';
