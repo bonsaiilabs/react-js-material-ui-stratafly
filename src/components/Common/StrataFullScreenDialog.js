d857e6b501c9dcbd6c1b94417a39d20dd19183f5
deekshasharma <deeksha.sharma25@gmail.com>
2019-07-21 15:54:41 -0700

    removed left transition

--- src/components/Common/StrataFullScreenDialog.js
+++ src/components/Common/StrataFullScreenDialog.js
@@ -1,28 +1,21 @@
 import React from 'react';
 import ArrowBack from '@material-ui/icons/ArrowBack';
-import { AppBar, Collapse, Typography, IconButton, Toolbar, Dialog } from '@material-ui/core';
-import Slide from '@material-ui/core/Slide';
+import { AppBar, Typography, IconButton, Toolbar, Dialog } from '@material-ui/core';
 
 export const StrataFullScreenDialog = ({ classes, label, open, onBack, children }) => {
   return (
-    <Collapse in={true}>
-      <Dialog fullScreen open={open} onClose={onBack} TransitionComponent={Transition}>
-        <AppBar position="fixed">
-          <Toolbar>
-            <IconButton color="inherit" onClick={onBack} aria-label="Close">
-              <ArrowBack />
-            </IconButton>
-            <Typography variant="h6" color="inherit">
-              {label}
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        {children}
-      </Dialog>
-    </Collapse>
+    <Dialog fullScreen open={open} onClose={onBack}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton color="inherit" onClick={onBack} aria-label="Close">
+            <ArrowBack />
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            {label}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      {children}
+    </Dialog>
   );
 };
-
-const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="left" ref={ref} {...props} />;
-});
