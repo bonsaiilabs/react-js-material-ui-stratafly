2323fd61da570e4b56cd02c382cf49ce4e5c0db7
deekshasharma <deeksha.sharma25@gmail.com>
2019-07-22 10:21:48 -0700

    Replaced ArrowBack with Close Image

--- src/components/Common/StrataFullScreenDialog.js
+++ src/components/Common/StrataFullScreenDialog.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ArrowBack from '@material-ui/icons/ArrowBack';
+import Close from '@material-ui/icons/Close';
 import { AppBar, Typography, IconButton, Toolbar, Dialog } from '@material-ui/core';
 
 export const StrataFullScreenDialog = ({ label, open, onBack, children }) => {
   return (
-    <Dialog fullScreen open={open} onClose={onBack}>
+    <Dialog fullScreen open={open}>
       <AppBar position="fixed">
         <Toolbar>
           <IconButton color="inherit" onClick={onBack} aria-label="Close">
-            <ArrowBack />
+            <Close />
           </IconButton>
           <Typography variant="h6" color="inherit">
             {label}
